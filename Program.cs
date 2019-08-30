f033dfc824706738f2e862cffeb5e5dd50f732d6
Ryan Smith <Rwsmith8692@gmail.com>
2019-08-30 15:21:52 -0500

    Practice Method

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Practice_Method
+{
+    class Program
+    {
+        public static string returnString()
+        {
+            return "This is a string";
+
+        }
+        public static int returnint()
+        {
+            return 3;
+        }
+
+            public static bool returnBool()
+        {
+            return true;
+        }
+
+    
+        static void Main(string[] args)
+        {
+            int myint = Program.returnint();
+            Console.WriteLine(myint);
+            {
+                string mystring = Program.returnString();
+                Console.WriteLine(mystring);
+                {
+                    bool mybool = Program.returnBool();
+                    Console.WriteLine(mybool);
+                }
+
+            }
+            
+             
+           
+        }
+    }
+}
